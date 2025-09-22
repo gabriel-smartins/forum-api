6a6fd5a7e4491962bf603516d3ce5f0698090359
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-22 15:03:45 -0300

    feat: add answer-question use case test

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
+
+describe("Answer Question Test", () => {
+  it("should create an answer", async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+    const answer = await answerQuestion.execute({
+      questionId: "1",
+      instructorId: "1",
+      content: "Nova Resposta",
+    });
+
+    expect(answer.content).toEqual("Nova Resposta");
+  });
+});
