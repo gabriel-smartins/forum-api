b5ce53d11504d409ac11a3723c24740d996318cc
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-23 13:53:44 -0300

    feat: update answer-question use case

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,24 +1,21 @@
-import { describe, it, expect } from "vitest";
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
-};
+  create: async (answer: Answer) => {},
+}
 
-describe("Answer Question Test", () => {
-  it("should create an answer", async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+describe('Answer Question Test', () => {
+  it('should create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
     const answer = await answerQuestion.execute({
-      questionId: "1",
-      instructorId: "1",
-      content: "Nova Resposta",
-    });
+      questionId: '1',
+      instructorId: '1',
+      content: 'Nova Resposta',
+    })
 
-    expect(answer.content).toEqual("Nova Resposta");
-  });
-});
+    expect(answer.content).toEqual('Nova Resposta')
+  })
+})
