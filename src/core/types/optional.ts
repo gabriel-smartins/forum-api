297b3309198dcd6ad6fe992e6e823fe3ddd7cf40
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-23 13:54:39 -0300

    feat: update entities

--- src/core/types/optional.ts
+++ src/core/types/optional.ts
@@ -13,6 +13,6 @@
  * ```
  **/
 
-import { T } from "vitest/dist/types-e3c9754d";
+import { T } from 'vitest/dist/types-e3c9754d'
 
-export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
