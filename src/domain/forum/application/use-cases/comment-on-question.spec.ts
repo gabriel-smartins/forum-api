722a693fd38d2033f25d049d10ceee4312b2b6a5
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-24 16:26:16 -0300

    feat: add comment-on-question use case; in-memory-question-comment-repository; question-comments-repository

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-question-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentRepository } from 'test/repositories/in-memory-question-comment-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Test Comment',
+    })
+
+    expect(inMemoryQuestionCommentRepository.items[0].content).toEqual(
+      'Test Comment',
+    )
+  })
+})
