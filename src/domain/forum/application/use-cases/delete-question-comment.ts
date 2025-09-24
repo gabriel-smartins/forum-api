a4c5075e5246ac71f56b9957d1306c5cd2397c8e
Gabriel Martins <gab.smartinn@outlook.com>
2025-09-24 17:00:20 -0300

    feat: add delete question comment use case

---
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -0,0 +1,32 @@
+import { QuestionCommentRepository } from '../repositories/question-comments-repository'
+
+interface DeleteQuestionCommentUseCaseRequest {
+  authorId: string
+  questionCommentId: string
+}
+
+interface DeleteQuestionCommentUseCaseResponse {}
+
+export class DeleteQuestionCommentUseCase {
+  constructor(private questionCommentRepository: QuestionCommentRepository) {}
+
+  async execute({
+    authorId,
+    questionCommentId,
+  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+    const questionComment =
+      await this.questionCommentRepository.findById(questionCommentId)
+
+    if (!questionComment) {
+      throw new Error('Question Comment not found')
+    }
+
+    if (questionComment.authorId.toString() !== authorId) {
+      throw new Error('Not Allowed')
+    }
+
+    await this.questionCommentRepository.delete(questionComment)
+
+    return {}
+  }
+}
